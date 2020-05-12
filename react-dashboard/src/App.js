dcb3528d0884e472f7819e1014973b9e29938e0e
Daniel <devdsfr@gmail.com>
2020-05-12 00:17:58 -0300

    01 Definindo estrutura

---
+++ react-dashboard/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import MenuSuperior from './components/MenuSuperior/MenuSuperior'
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <MenuSuperior/>
+      </div>
+    );
+  }
+}
+export default App;
