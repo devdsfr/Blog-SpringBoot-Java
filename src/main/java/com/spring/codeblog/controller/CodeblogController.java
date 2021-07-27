3ed32460d2680b31b3adeceb861f921420f28c7e
Daniel Henrique Jorge Ramos <daniel.henrique@totvs.com.br>
2021-07-27 19:56:18 -0300

    07: Paginação do blog

--- src/main/java/com/spring/codeblog/controller/CodeblogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -4,6 +4,7 @@ import com.spring.codeblog.model.Post;
 import com.spring.codeblog.service.CodeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,6 +22,14 @@ public class CodeblogController {
         ModelAndView mv = new ModelAndView("posts");
         List<Post> posts = codeblogService.findAll();
         mv.addObject("posts", posts);
+        return mv;
+    }
+
+    @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+    public ModelAndView getPostsDetails(@PathVariable("id") long id){
+        ModelAndView mv = new ModelAndView("postsDetails");
+        Post post = codeblogService.findById(id);
+        mv.addObject("post", post);
         return mv;
     }
 }
